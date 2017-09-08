960dec8cf670aeb1aebee34699f5d7bd18b4fdac
Epig <wigo118@hotmail.de>
2017-09-08 17:33:30 +0200

    Changed int sleep to unsigned int sleep

--- compat.h
+++ compat.h
@@ -49,7 +49,7 @@ static inline int nanosleep(const struct timespec *req, struct timespec *rem)
 
 #if defined(__MINGW32__) && !defined(__MINGW64_VERSION_MAJOR)
 // Reported unneded in https://github.com/veox/sgminer/issues/37 */
-static inline int sleep(unsigned int secs)
+static inline unsigned int sleep(unsigned int secs)
 {
 	struct timespec req, rem;
 	req.tv_sec = secs;
